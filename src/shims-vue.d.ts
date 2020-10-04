0a367cb728a71c062d566116895c5c909ef1d463
Baiyue Li <liby1994@126.com>
2020-10-04 23:11:01 +0800

    feature initial vue project

--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -3,3 +3,10 @@ declare module "*.vue" {
   const component: ReturnType<typeof defineComponent>;
   export default component;
 }
+/*
+TypeScript 默认并不支持 *.vue 后缀的文件
+所以在 vue 项目中引入的时候需要创建一个shims-vue.d.ts 文件
+放在项目项目对应使用目录下
+例如 src/shims-vue.d.ts
+用来支持*.vue 后缀的文件
+*/
